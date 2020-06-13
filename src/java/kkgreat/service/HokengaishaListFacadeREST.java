88ce7745d6132c4a65af7db69a1e8b33104674c6
great_kaneko <great_kaneko@localhost>
2020-06-14 07:08:41 +0900

    メンテナンス保険会社リスト用クエリ、メソッド追加

--- src/java/kkgreat/service/HokengaishaListFacadeREST.java
+++ src/java/kkgreat/service/HokengaishaListFacadeREST.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 保険会社リストサービスクラス
+ * 
+ * 
  */
 package kkgreat.service;
 
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,11 +19,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import kkgreat.HokengaishaList;
+import kkgreat.Kankyou;
 
-/**
- *
- * @author great_kaneko
- */
+
 @Stateless
 @Path("hokengaishalist")
 public class HokengaishaListFacadeREST extends AbstractFacade<HokengaishaList> {
@@ -41,6 +40,72 @@ public class HokengaishaListFacadeREST extends AbstractFacade<HokengaishaList> {
     public List<HokengaishaList> findAll() {
         return super.findAll();
     }
+    
+    /*
+    * 保険会社名検索(1件)
+    *
+    */
+    @TokenSecurity
+    @GET
+    @Path("getbyhokengaishaname/{name}")
+    @Produces({"application/json"})
+    public List<HokengaishaList> getByHokengaishaName(@PathParam("name") String name) {
+        TypedQuery<HokengaishaList> q = getEntityManager().createNamedQuery("HokengaishaList.findByHokengaisha", HokengaishaList.class);
+        q.setParameter("hokengaisha", name);
+        return q.getResultList();
+    }
+    
+    /*
+    * 保険会社登録
+    *
+    */
+    @TokenSecurity
+    @POST
+    @Path("create")
+    @Consumes({"application/json"})
+    @Produces({"application/json"})
+    public HokengaishaList createHokengaishaList(HokengaishaList entity) {
+        /*
+        *   idの割り当ては、Kankyouテーブルhokengaisha_maxより取得し、HokengaishaListにInsert時に同時に更新している
+        */
+        Kankyou kankyou = getEntityManager().find(Kankyou.class, 1L);
+        Long maxId = kankyou.getHokengaishaMax();
+        maxId += 1L;
+        entity.setId(maxId);
+        super.create(entity);
+        super.flush();                            //継承クラスにflushメソッドを追加、entityManagerに即時insert発行
+        kankyou.setHokengaishaMax(maxId);         //Kankyoテーブルhokengaisha_max値を更新
+        getEntityManager().merge(kankyou);
+        super.flush();
+        return super.find(entity.getId());
+    }
+    
+    /*
+    * 保険会社更新
+    *
+    */
+    @TokenSecurity
+    @POST
+    @Path("update")
+    @Consumes({"application/json"})
+    public HokengaishaList update(HokengaishaList entity) {
+        super.edit(entity);
+        return super.find(entity.getId());
+    }
+    
+    /*
+    * 保険会社削除
+    *
+    */
+    @TokenSecurity
+    @DELETE
+    @Path("delete/{id}")
+    @Produces("text/plain")
+    public Integer remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+        return 1;
+    }
+    
     
     /*
     * 以下未使用メソッド
