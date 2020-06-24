d9a052710471905fbfee298782f120d80483758e
great_kaneko <great_kaneko@localhost>
2020-06-24 17:43:09 +0900

    コメント修正

--- src/java/kkgreat/Kanri.java
+++ src/java/kkgreat/Kanri.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 書類管理テーブルEntity
  */
 package kkgreat;
 
